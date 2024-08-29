a490e3d284dacaa6adc79f0b82a4497686e58a27
Leandro <leandrosarkis1602@yahoo.com>
2024-08-29 16:45:39 -0300

    Finalizando o projetp

--- scripts.js
+++ scripts.js
@@ -9,6 +9,7 @@ const amount = document.getElementById("amount");
 const currency = document.getElementById("currency");
 const footer = document.querySelector("main footer");
 const description = document.getElementById("description");
+const result = document.getElementById("result");
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -36,6 +37,18 @@ form.onsubmit = (event) => {
 // Função para converter a moeda.
 function convertCurrency(amount, price, symbol) {
     try {
+        // Exibindo a cotação da moeda selecionada.
+        description.textContent = `${symbol} 1 = ${formatCurrencyBRL(price)}`;
+
+        // Calcula o total
+        let total = amount * price;
+
+        // Formatar o valor total.
+        total = formatCurrencyBRL(total).replace("R$", "");
+
+        // Exibe o resultado total
+        result.textContent = `${total} Reais`;
+
         // Aplica a classe que exibe o footer para mostrar o resultado.
         footer.classList.add("show-result");
     } catch (error) {
@@ -45,4 +58,12 @@ function convertCurrency(amount, price, symbol) {
         console.log(error);
         alert("Não foi possível converter. Tente novamente mais tarde");
     }
-}
+};
+
+// Formata a moeda em Real Brasileiro.
+function formatCurrencyBRL(value) {
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    });
+};
